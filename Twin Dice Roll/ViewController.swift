e17917ee4e66c98c6021423e3fe44e4e3de15f3b
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-01-15 17:11:17 +0300

    Initial Commit

---
+++ Twin Dice Roll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twin Dice Roll
+//
+//  Created by Muhammed Mustafa Geldi on 15.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Twin Dice RollTests/Twin_Dice_RollTests.swift
@@ -0,0 +1,17 @@
+//
+//  Twin_Dice_RollTests.swift
+//  Twin Dice RollTests
+//
+//  Created by Muhammed Mustafa Geldi on 15.01.2025.
+//
+
+import Testing
+@testable import Twin_Dice_Roll
+
+struct Twin_Dice_RollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
